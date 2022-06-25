8b9120290b9f5ca537069f5a5f003e7222aa67be
bloushi22 <107666401+bloushi22@users.noreply.github.com>
2022-06-25 19:08:54 +0300

    cw2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿string heroName = "Link";
+int heroPower = 550;
+
+string villainName = "Ganon";
+int villainPower = 500;
+
+
+if (heroPower > villainPower)
+{
+    Console.WriteLine("the strongest is " + heroName);
+}
+
+else
+{
+    Console.WriteLine("the strongest is " + villainName);
+        }
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿float playerspeed = 1.5f;
+Console.WriteLine(playerspeed);
+void SetSpeed() {
+    playerspeed = 2.5f;
+}
+Console.WriteLine(playerspeed);
+
+SetSpeed();
+Console.WriteLine(playerspeed);
